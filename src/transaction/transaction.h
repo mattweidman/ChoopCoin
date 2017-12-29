71111b08e7c942f90321ffba86be7627f15a38ba
Matt <weidman.matthew@gmail.com>
2017-12-28 22:41:19 -0700

    transaction.h

--- src/transaction/transaction.h
+++ src/transaction/transaction.h
@@ -2,8 +2,60 @@
 #define CHOOP_TRANSACTION_H
 
 #include <string>
+#include <vector>
+
+#include "utxo.h"
+
+class TxnInput : Serializable {
+
+  private:
+    /** Unspent transaction: public key + denomination */
+    UTXO utxo;
+    /** Signature of UTXO that can be verified using key */
+    std::string signature;
+
+  public:
+    /**
+     * utxo: unspent transaction output for this input
+     * signature: signature of utxo
+     */
+    TxnInput(UTXO utxo, std::string signature);
+
+    /** Get UTXO of input */
+    UTXO getUTXO();
+
+    /** Get signature of utxo */
+    std::string getSignature();
+
+    /** Convert to string */
+    std::string serialize();
+
+    /** 
+     * Verify the following:
+     * - transaction is signed by holder of public key
+     * - denomination > 0
+     */
+    bool verify();
+
+};
 
 class Transaction {
+
+  private:
+    /** List of inputs */
+    std::vector<TxnInput> inputs;
+    /** List of outputs */
+    std::vector<UTXO> outputs;
+
+  public: 
+    /** Add to list of donors */
+    void addInput(TxnInput);
+
+    /** Add to list of recipients */
+    void addOutput(UTXO);
+
+    /** Convert to string */
+    std::string serialize();
 
 };
 
